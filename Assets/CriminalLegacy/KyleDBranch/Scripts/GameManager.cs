3637c1de12e4bbf6d0402791c5887e036b6d6213
ThatOnePerson132 <118837311+ThatOnePerson132@users.noreply.github.com>
2023-03-08 09:56:26 -0800

    wasted time on code and slight setup of UI

--- Assets/CriminalLegacy/KyleDBranch/Scripts/GameManager.cs
+++ Assets/CriminalLegacy/KyleDBranch/Scripts/GameManager.cs
@@ -6,12 +6,28 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+
+    public int health;
+    public int stamina;
+    public int bananasCollected;
+
+    public TextMeshProUGUI healthDisplay;
+    public TextMeshProUGUI staminaDisplay;
+    public TextMeshProUGUI bananaMeter;
+
     public bool isGameActive;
 
     // Start is called before the first frame update
     void Start()
     {
+        health = 100;
+        healthDisplay.text = "Health: " + health + "/100";
         
+        stamina = 100;
+        staminaDisplay.text = "Stamina: " + stamina + "/100";
+
+        bananasCollected = 0;
+        bananaMeter.text = "BananasCollected: " + bananasCollected;
     }
 
     // Update is called once per frame
